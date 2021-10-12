5f8b79ba50527d517e8158d09eb07c25e71cb431
pabaier <baierpa@g.cofc.edu>
2021-10-12 11:15:01 -0400

    create user class and access tables

--- tables.sql
+++ tables.sql
@@ -2,15 +2,28 @@ CREATE TABLE users (
   id INTEGER PRIMARY KEY autoincrement,
   first_name text,
   last_name text,
-  email text,
-  section smallint
+  email text
 );
 
 CREATE UNIQUE INDEX users_email ON users(email);
 CREATE UNIQUE INDEX users_section ON users(section);
 
+CREATE TABLE user_class (
+  user_id int,
+  class_id text,
+  FOREIGN KEY(user_id) REFERENCES users(id)
+);
+
+CREATE TABLE user_access (
+  user_id int,
+  access_type text,
+  FOREIGN KEY(user_id) REFERENCES users(id)
+);
+
+
 CREATE TABLE attendance (
   user_id int,
+  comment text,
   date_created DATETIME DEFAULT (DATETIME('now')),
   FOREIGN KEY(user_id) REFERENCES users(id)
 );
