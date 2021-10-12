ca9fdff974441e3556834f82a8242f601e6312cd
pabaier <baierpa@g.cofc.edu>
2021-10-12 13:27:14 -0400

    create course table

--- tables.sql
+++ tables.sql
@@ -1,23 +1,34 @@
-CREATE TABLE users (
+CREATE TABLE user (
   id INTEGER PRIMARY KEY autoincrement,
   first_name text,
   last_name text,
   email text
 );
 
-CREATE UNIQUE INDEX users_email ON users(email);
-CREATE UNIQUE INDEX users_section ON users(section);
+CREATE UNIQUE INDEX user_email ON user(email);
+CREATE UNIQUE INDEX user_section ON user(section);
 
-CREATE TABLE user_class (
+CREATE TABLE course (
+  id INTEGER PRIMARY KEY autoincrement,
+  name text,
+  number int,
+  subject text,
+  section int
+);
+
+CREATE TABLE user_course (
   user_id int,
-  class_id text,
-  FOREIGN KEY(user_id) REFERENCES users(id)
+  course_id int,
+  semester int,
+  FOREIGN KEY(user_id) REFERENCES user(id),
+  FOREIGN KEY(course_id) REFERENCES course(id),
+  UNIQUE(user_id, course_id, semester)
 );
 
 CREATE TABLE user_access (
   user_id int,
   access_type text,
-  FOREIGN KEY(user_id) REFERENCES users(id)
+  FOREIGN KEY(user_id) REFERENCES user(id)
 );
 
 
@@ -25,7 +36,7 @@ CREATE TABLE attendance (
   user_id int,
   comment text,
   date_created DATETIME DEFAULT (DATETIME('now')),
-  FOREIGN KEY(user_id) REFERENCES users(id)
+  FOREIGN KEY(user_id) REFERENCES user(id)
 );
 
 CREATE INDEX attendance_date ON attendance(date_created);
