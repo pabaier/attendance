b445c5e0f8b921895d094dc460a22bd0ec669462
pabaier <baierpa@g.cofc.edu>
2022-06-01 15:15:16 -0400

    use session

--- index.ts
+++ index.ts
@@ -1,10 +1,22 @@
 import express, { Express, Request, Response } from 'express';
 const dotenv = require('dotenv');
+const session = require('express-session')
+const MemoryStore = require('memorystore')(session)
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT;
+
+app.use(session({
+  cookie: { maxAge: 86400000 },
+  store: new MemoryStore({
+    checkPeriod: 86400000 // prune expired entries every 24h
+  }),
+  resave: false,
+  secret: 'keyboard cat',
+  saveUninitialized: true
+}))
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Express + TypeScript Server');
