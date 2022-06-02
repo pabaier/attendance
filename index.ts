425af47112430d8121671ebc077efa920f5215a6
pabaier <baierpa@g.cofc.edu>
2022-06-01 21:19:34 -0400

    use ejs views

--- index.ts
+++ index.ts
@@ -1,13 +1,25 @@
 import express, { Express, Request, Response } from 'express';
-const dotenv = require('dotenv');
-const session = require('express-session')
+import dotenv from 'dotenv';
+import session from 'express-session';
 const MemoryStore = require('memorystore')(session)
+import expressLayouts from 'express-ejs-layouts';
+import path from 'path';
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT;
 
+// EJS
+app.use(expressLayouts)
+// Setting the root path for views directory
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, "public")));
+// Setting the view engine
+app.set('view engine', 'ejs');
+// console.log(__dirname)
+
+// SESSIONS
 app.use(session({
   cookie: { maxAge: 86400000 },
   store: new MemoryStore({
@@ -18,8 +30,12 @@ app.use(session({
   saveUninitialized: true
 }))
 
+// **********************************************************************************************
+
 app.get('/', (req: Request, res: Response) => {
-  res.send('Express + TypeScript Server');
+  res.render('index.ejs', { title: 'Attendance'})
+
+  // res.send('Express + TypeScript Server');
 });
 
 app.listen(port, () => {
