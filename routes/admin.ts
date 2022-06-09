a1696c08b534764c272722c72a35a66d8a709d03
pabaier <baierpa@g.cofc.edu>
2022-06-09 01:07:48 -0400

    do not repeat consecutive numbers in code

--- routes/admin.ts
+++ routes/admin.ts
@@ -19,8 +19,12 @@ export default function (myCache: NodeCache, dbClient: DbClient) {
     });
 
     router.get('/code/update', (req: Request, res: Response) => {
-        const newNumber: number = Math.floor(Math.random() * 9) + 1;
         const value: number = myCache.has('code') ? myCache.take('code') as number : 0;
+        const oldNumber: number = value % 10;
+        var newNumber: number = Math.floor(Math.random() * 9) + 1;
+        while (newNumber == oldNumber) {
+            newNumber = Math.floor(Math.random() * 9) + 1;
+        }
         myCache.set('code', (value * 10 + newNumber) % 100000, codeRefreshRate / 1000 + 500);
         res.json({ code: newNumber })
     });
