491f97f26b6a0d31eed4e8706295ebde1e5e5de2
pabaier <paul.a.baier@gmail.com>
2022-09-13 14:55:42 -0400

    list posts chronologically

--- routes/user.ts
+++ routes/user.ts
@@ -30,16 +30,32 @@ export default function (myCache: NodeCache, dbClient: DbClient) {
 
     router.get('/announcements', async (req: Request, res: Response) => {
         const userGroupIds = req.session.user?.groups as number[];
-        const posts: PostGroup[][] = []
+        var posts: PostGroup[] = []
         const today = new Date();
         for (const id of userGroupIds) {
             const unfilteredPosts: PostGroup[] = await dbClient.getPosts(id);
-            posts.push(unfilteredPosts.filter(post => post.visible).map(post => {
+            posts = posts.concat(unfilteredPosts.filter(post => post.visible).map(post => {
                 post.link = post.openTime < today ? post.link : ''
                 return post
             }))
         }
-        res.render('user/posts', { posts })
+
+        // earlier items in the list will show higher, which is what we want
+        // so they need to register as "less than" (-1) during the sort
+        // closed items always show lower in the list
+        const p = posts.sort((a: PostGroup, b:PostGroup) => {
+            const aIsEarlier = a.openTime < b.openTime;
+            const aIsOpen = a.openTime < today;
+            const bIsOpen = b.openTime < today;
+            if (aIsOpen && bIsOpen && aIsEarlier) return 1 // b shows up earlier/higher
+            if (aIsOpen && bIsOpen && !aIsEarlier) return -1 // a shows up earlier/higher
+            if (aIsOpen && !bIsOpen) return -1; // a shows up earlier/higher
+            if (!aIsOpen && bIsOpen) return 1; //b shows up earlier/hight
+            if (!aIsOpen && !bIsOpen && aIsEarlier) return 1
+            if (!aIsOpen && !bIsOpen && !aIsEarlier) return -1 
+            return 0
+        })
+        res.render('user/posts', { posts: p })
     });
 
     router.post('/attendance', async (req: Request, res: Response) => {
