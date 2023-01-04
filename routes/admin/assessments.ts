a6a4af33fd6d75fba82c95a787942cb93acadd7c
pabaier <paul.a.baier@gmail.com>
2023-01-04 09:05:41 -0500

    crud assessment settings

--- routes/admin/assessments.ts
+++ routes/admin/assessments.ts
@@ -1,6 +1,6 @@
 import express, { Request, Response } from 'express';
 import { DbClient } from '../../db/dbClient';
-import { Assessment, AssessmentQuestion, Post, PostGroup } from '../../models';
+import { Assessment, AssessmentQuestion, AssessmentSettings, Post, PostGroup } from '../../models';
 import { renderFile } from '../../views/helper';
 import { makeUTCDateString } from '../helpers';
 
@@ -29,13 +29,18 @@ export default function (dbClient: DbClient) {
             return { id: q.id, selected: testQuestions.some(x => x.questionId == q.id) }
         })
 
-        // get assessment settings
-        var assessmentSettings = await dbClient.getAssessmentSettings(assessmentId);
+        var assessmentSettings: (AssessmentSettings & { groupName: string} )[] = await dbClient.getAssessmentSettings(assessmentId);
+        var settings = assessmentSettings.map(as => {
+            return {
+                ...as,
+                startTime: as.startTime ? makeUTCDateString(as.startTime) : undefined,
+                endTime: as.endTime ? makeUTCDateString(as.endTime) : undefined,
+            }
+        })
+        const groups = await dbClient.getGroupsNotPartOfAssessment(assessmentId)
+        const groupsDropdown = renderFile('./views/admin/partials/group-select-dropdown.ejs', { groups, selected: 0, id: 0 });
 
-        // get all questions then add multiselect to add questions to assessment
-        // assign assessment to different groups
-
-        res.render('admin/assessments/assessment', { assessment, testQuestions, questions, settings: assessmentSettings });
+        res.render('admin/assessments/assessment', { assessment, testQuestions, questions, settings, groupsDropdown });
     });
 
     router.put('/:assessmentId', async (req: Request, res: Response) => {
@@ -43,6 +48,34 @@ export default function (dbClient: DbClient) {
         var name = req.body.name;
         const success = await dbClient.updateAssessment({ id: assessmentId, name });
         success ? res.status(200).send({message: `Assessment ${assessmentId} updated`}) : res.status(500).send({message: 'error'});
+    });
+
+    router.post('/:assessmentId/settings', async (req: Request, res: Response) => {
+        var assessmentId = parseInt(req.params.assessmentId)
+        var groupId = parseInt(req.body.groupId);
+        var startTime = req.body.start ? new Date(req.body.start) : undefined;
+        var endTime = req.body.end ? new Date(req.body.end) : undefined;
+
+        const success = await dbClient.createAssessmentSettings({ assessmentId, groupId, startTime, endTime })
+        success ? res.status(200).send({message: `Assessment Setting Created`}) : res.status(500).send({message: 'error'});
+    });
+
+    router.delete('/:assessmentId/settings', async (req: Request, res: Response) => {
+        var assessmentId = parseInt(req.params.assessmentId)
+        var groupId = parseInt(req.body.groupId);
+
+        const success = await dbClient.deleteAssessmentSettings(assessmentId, groupId)
+        success ? res.status(200).send({message: `Assessment Deleted`}) : res.status(500).send({message: 'error'});
+    });
+
+    router.put('/:assessmentId/settings', async (req: Request, res: Response) => {
+        var assessmentId = parseInt(req.params.assessmentId)
+        var groupId = parseInt(req.body.groupId);
+        var startTime = req.body.start ? new Date(req.body.start) : undefined;
+        var endTime = req.body.end ? new Date(req.body.end) : undefined;
+
+        const success = await dbClient.updateAssessmentSettings({ assessmentId, groupId, startTime, endTime })
+        success ? res.status(200).send({message: `Assessment Settings Updated`}) : res.status(500).send({message: 'error'});
     });
 
     router.put('/:assessmentId/question', async (req: Request, res: Response) => {
